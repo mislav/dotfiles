03719e33f101869e8a95db9a81377fd4c46b4a2c
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-01-14 03:08:19 +0100

    add `nginx-dir` for starting homebrewed nginx w/ Passenger per-project
      $ brew install nginx --with-passenger

--- install.rb
+++ install.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-
 # from http://errtheblog.com/posts/89-huba-huba
 
 home = ENV['HOME']
@@ -8,7 +7,7 @@ Dir.chdir File.dirname(__FILE__) do
   dotfiles_dir = Dir.pwd.sub(home + '/', '')
   
   Dir['*'].each do |file|
-    next if file == 'install.rb'
+    next unless File.extname(file).empty?
     target_name = file == 'bin' ? file : ".#{file}"
     target = File.join(home, target_name)
     unless File.exist? target
