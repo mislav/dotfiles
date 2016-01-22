14df4bbf07a8f33152b3a9732558210855f4633b
Mislav Marohnić <mislav.marohnic@gmail.com>
2016-01-22 17:46:20 +1100

    Remove outdated `~/.shrc` stuff that zshrc still references

--- shrc/editor.sh
+++
@@ -1,8 +0,0 @@
-export EDITOR=vim
-export GIT_EDITOR=$EDITOR
-
-if [[ -z $TMUX ]]; then
-  # wanted to set VISUAL here, but it affects too many things I don't want
-  [[ "$(uname -s)" == Darwin ]] && GEM_EDITOR=mvim || GEM_EDITOR=gvim
-  export GEM_EDITOR
-fi
--- shrc/process.sh
+++
@@ -1,4 +0,0 @@
-# mojombo http://gist.github.com/180587
-function psg {
-  ps wwwaux | egrep "($1|%CPU)" | grep -v grep
-}
